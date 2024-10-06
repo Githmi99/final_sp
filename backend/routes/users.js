005e4c03f018d4816e74d74a6f92e6d5eb5fe816
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-10-06 08:18:22 +0530

    change

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,7 +21,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password, name } = req.body;
+    const { email, password, name, role } = req.body;
 
     try {
       let user = await User.findOne({ email });
@@ -33,6 +33,7 @@ router.post(
         email,
         password,
         name,
+        role,
       });
 
       const salt = await bcrypt.genSalt(10);
@@ -46,15 +47,10 @@ router.post(
         },
       };
 
-      jwt.sign(
-        payload,
-        process.env.JWT_SECRET,
-        { expiresIn: '5h' },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      jwt.sign(payload, 'WESFRGH', { expiresIn: '5h' }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
@@ -92,18 +88,14 @@ router.post(
       const payload = {
         user: {
           id: user.id,
+          role: user.role,
         },
       };
 
-      jwt.sign(
-        payload,
-        process.env.JWT_SECRET,
-        { expiresIn: '5h' },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      jwt.sign(payload, 'WESFRGH', { expiresIn: '5h' }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
