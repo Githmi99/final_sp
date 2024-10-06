0b77ec9162a0775d5a8512aa8212403292b9d3ec
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-10-06 07:56:15 +0530

    user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -35,6 +35,7 @@ router.post('/login', async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
+    console.log(user);
     if (!user) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
