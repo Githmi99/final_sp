0bb30f58045ee7f316345e6201dcc94690b656c0
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-10-06 08:56:59 +0530

    logout

--- stockpulse-frontend/src/components/Header.js
+++ stockpulse-frontend/src/components/Header.js
@@ -17,31 +17,44 @@ const Header = ({ title, titlePrefix = 'My' }) => {
   };
 
   const handleLogout = () => {
-    // Optional: Clear authentication tokens or user data
-    localStorage.removeItem('token'); // Example for JWT token
+    localStorage.removeItem('token'); // Assuming you store the token in localStorage
 
-    // Navigate to login page
-    navigate('/loginpage'); // Adjust this path if necessary
+    // Clear the role from localStorage if stored
+    localStorage.removeItem('role');
+
+    // Clear the authentication token or other cookies by setting them to expire in the past
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; // Clear 'authToken' cookie
+    document.cookie = 'role=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; // Clear 'role' cookie
+
+    navigate('/'); // Adjust this path if necessary
   };
 
   return (
-    <div className="header-container">
-      <div className="title">
-        <span className="bold">{titlePrefix}</span> <span className="highlight">{title}</span>
+    <div className='header-container'>
+      <div className='title'>
+        <span className='bold'>{titlePrefix}</span>{' '}
+        <span className='highlight'>{title}</span>
       </div>
-      <div className="search-bar">
-        <input type="text" placeholder="Search here" />
-        <button className="search-button">
-          <i className="fa fa-search"></i> Search
+      <div className='search-bar'>
+        <input type='text' placeholder='Search here' />
+        <button className='search-button'>
+          <i className='fa fa-search'></i> Search
         </button>
       </div>
-      <div className="notifications">
-        <button className="notification-button" onClick={toggleNotifications}>
+      <div className='notifications'>
+        <button className='notification-button' onClick={toggleNotifications}>
           <FaBell />
         </button>
-        {showNotifications && <NotificationPanel visible={showNotifications} onClose={closeNotifications} />}
+        {showNotifications && (
+          <NotificationPanel
+            visible={showNotifications}
+            onClose={closeNotifications}
+          />
+        )}
       </div>
-      <button className="logout-btn" onClick={handleLogout}>Log Out</button>
+      <button className='logout-btn' onClick={handleLogout}>
+        Log Out
+      </button>
     </div>
   );
 };
